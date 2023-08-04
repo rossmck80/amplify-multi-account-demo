8c4a228e173d57e7a918f6cfd00a90d1b6778664
Ross McKillop <rossmck@amazon.com>
2023-08-04 14:59:49 +0800

    Removed backend

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,15 +2,15 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 import { test, expect } from "vitest";
 
-// test("renders Hello in title", () => {
-//   render(<App />);
-//   const helloTitle = screen.getByText(/Hello/i);
-//   expect(helloTitle).toBeInTheDocument();
-// });
-
-// failed tests
 test("renders Hello in title", () => {
   render(<App />);
-  const helloTitle = screen.getByText(/Hi/i);
+  const helloTitle = screen.getByText(/Hello/i);
   expect(helloTitle).toBeInTheDocument();
 });
+
+// // failed tests
+// test("renders Hello in title", () => {
+//   render(<App />);
+//   const helloTitle = screen.getByText(/Hi/i);
+//   expect(helloTitle).toBeInTheDocument();
+// });
